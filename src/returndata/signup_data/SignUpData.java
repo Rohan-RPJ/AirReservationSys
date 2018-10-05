ea6482b5470785fbfef3182df86988f86d4f9828
Rohan <>
2018-10-06 00:59:43 +0530

    SignUpData returnpage

---
+++ src/returndata/signup_data/SignUpData.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package returndata.signup_data;
+
+import window.java.Sign_Up_Window;
+
+/**
+ *
+ * @author Rohan
+ */
+public class SignUpData extends Sign_Up_Window {
+    
+    public String return_mob_no()
+    {
+        return mobile_no_tf.getText();
+    }
+    
+}
+
+
