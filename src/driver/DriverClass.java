48d9220cf346012dd090424321d27375cf6d53b7
DG <dgdivyanshu@gmail.com>
2018-10-08 01:04:30 +0530

    Added driverclass(non functional)

--- src/driver/DriverClass.java
+++ src/driver/DriverClass.java
@@ -6,16 +6,58 @@
 
 package driver;
 
+import java.io.FileInputStream;
 import java.sql.*;
-
+import java.util.Properties;
+import window.java.User;
 /**
  *
  * @author akgy2
  */
 public class DriverClass {
+    //stores the status of the operation
     private int flag;
+    //stores the User object returned by getFormDetails()
+    private User u=new User();
+    private Connection con;
+    private Statement st;
     
-    public setFlag(int status)
+    DriverClass(){
+        con =null;
+        st= null;
+        try{
+            
+            //registering the driver
+            Class.forName("com.mysql.jdbc.Driver");
+            
+        }
+        catch(ClassNotFoundException e)
+                {
+            System.out.println("Driver registration error!");
+            System.exit(1);
+        }
+        try{
+            //loading the properties file
+            Properties prop = new Properties();
+            prop.load(new FileInputStream("userdb.properites"));
+            
+            String user = prop.getProperty("user");
+            String password= prop.getProperty("password");
+            String dburl= prop.getProperty("dburl");
+            
+            con= DriverManager.getConnection(dburl, user, password);
+        
+                }
+        catch(Exception e){
+        
+        }
+    }
+    public void setUserData(User u)
+    {
+        this.u=u;
+    }
+    
+    public void setFlag(int status)
     {
         flag =  status;
     }
