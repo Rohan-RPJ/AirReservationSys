be8b24a36aae0d8fd9ee7f9183b37c1631d12cf6
Rohan <>
2018-10-12 00:49:11 +0530

    updated flights search

--- src/window/java/Flights.java
+++ src/window/java/Flights.java
@@ -9,10 +9,19 @@ import javafx.application.Application;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -35,6 +44,8 @@ public class Flights extends Application{
         Traveller t = new Traveller();
         
         bp = new BorderPane();
+        
+        //**** Top of Border Pane ****//
         top_gp = new GridPane();
         
         top_gp.setGridLinesVisible(true); 
@@ -62,8 +73,8 @@ public class Flights extends Application{
         infant_lbl =new Label("Infant");
         
         top_gp.add(adult_lbl, 3, 3);
-        top_gp.add(child_lbl, 4, 3);
-        top_gp.add(infant_lbl, 5, 3);
+        top_gp.add(child_lbl, 5, 3);
+        top_gp.add(infant_lbl, 7, 3);
         //
         
         //
@@ -76,12 +87,283 @@ public class Flights extends Application{
         tot_infant = new Label(infants);
         GridPane.setHalignment(tot_infant, HPos.CENTER);
         
-        top_gp.add(tot_adult, 3, 4);
-        top_gp.add(tot_child, 4, 4);
-        top_gp.add(tot_infant, 5, 4);
-        //
+        top_gp.add(tot_adult, 4, 3);
+        top_gp.add(tot_child, 6, 3);
+        top_gp.add(tot_infant, 8, 3);
         
+        top_gp.setStyle("-fx-background-color: #999999;");
+        //End of top of  borderPane 
+        
+        //**** Center Of bottom Pane ****//
+        
+        HBox hb = new HBox(20);
+        
+        GridPane[] center_gp1 = new GridPane[4];
+        VBox v1 = new VBox(30);
+        StackPane[] sp1 = new StackPane[4];
+        Rectangle[] r1 = new Rectangle[4];
+        Image[] logo1 = new Image[4];
+        ImageView[] logo_img_view1 = new ImageView[4];
+        Label[] flight_name1 = new Label[4];
+        Label[] time_src1 = new Label[4];
+        Label[] time_dest1 = new Label[4];
+        Label[] source1 = new Label[4];
+        Label[] destination1 = new Label[4];
+        Label[] tot_time_req1 = new Label[4];
+        Label[] fare1 = new Label[4];
+        Button[] book_btn1 = new Button[4];
+       
+        GridPane[] center_gp2 = new GridPane[4];
+        VBox v2 = new VBox(30);
+        StackPane[] sp2 = new StackPane[4];
+        Rectangle[] r2 = new Rectangle[4];
+        Image[] logo2 = new Image[4];
+        ImageView[] logo_img_view2 = new ImageView[4];
+        Label[] flight_name2 = new Label[4];
+        Label[] time_src2 = new Label[4];
+        Label[] time_dest2 = new Label[4];
+        Label[] source2 = new Label[4];
+        Label[] destination2 = new Label[4];
+        Label[] tot_time_req2 = new Label[4];
+        Label[] fare2 = new Label[4];
+        Button[] book_btn2 = new Button[4];
+        int i=0;
+        //Passenger Details of no. of adults selected 
+        try{
+        //int tot_fligts = Integer.parseInt(); 
+        
+        for(; i<4; i++)
+        {
+            
+            //setting primary properties required for gridPane
+            center_gp1[i] = new GridPane();
+            center_gp1[i].setAlignment(Pos.CENTER); 
+            
+            //background of gridPane
+            r1[i] = new Rectangle();
+            r1[i].setWidth(530); 
+            r1[i].setHeight(230);
+            r1[i].setStyle("-fx-fill:white;-fx-effect: innershadow( three-pass-box , rgba(0,0,0,0.7) , 6, 0.0 , 0 , 2 ); ");
+            
+            
+            //center_gp[i].setStyle("-fx-background-color: white");
+            //setting gaps between rows and columns of grid 
+            center_gp1[i].setHgap(10);
+            center_gp1[i].setVgap(10);
+            
+            //padding of gridpane in scene
+            center_gp1[i].setPadding(new Insets(5,5,5,5));
+        
+            //center_gp[i].setGridLinesVisible(true);
+            
+            //end of setting
+            
+            //contents inside gridPane
+            
+            logo1[i] = new Image(Window.class.getResourceAsStream("air-logo.png"));
+            logo_img_view1[i] = new ImageView(logo1[i]);
+        
+           
+            //setting size of image logo
+            logo_img_view1[i].setFitHeight(65);
+            logo_img_view1[i].setFitWidth(65);
+            GridPane.setHalignment(logo_img_view1[i], HPos.CENTER);
+            GridPane.setValignment(logo_img_view1[i], VPos.CENTER);
+            //adding logo image 
+            center_gp1[i].add(logo_img_view1[i],0,0);
+            
+            //Adding flight name 
+            flight_name1[i] = new Label("AeroSwing");
+            flight_name1[i].setStyle("-fx-font-size: 15px;");
+            GridPane.setHalignment(flight_name1[i], HPos.LEFT); 
+            GridPane.setValignment(flight_name1[i], VPos.CENTER);
+            center_gp1[i].add(flight_name1[i], 1, 0);
+            
+            time_src1[i] = new Label("17:10");
+            time_src1[i].setStyle("-fx-font-size: 30px;-fx-font-weight: bold;");
+            GridPane.setHalignment(time_src1[i], HPos.CENTER); 
+            center_gp1[i].add(time_src1[i], 4, 0);
+            
+            time_dest1[i] = new Label("19:25");
+            time_dest1[i].setStyle("-fx-font-size: 30px;-fx-font-weight: bold;");
+            GridPane.setHalignment(time_dest1[i], HPos.CENTER); 
+            center_gp1[i].add(time_dest1[i], 7, 0);
+            
+            source1[i] = new Label(src);
+            source1[i].setStyle("-fx-font-size: 20px;"); 
+            GridPane.setHalignment(source1[i], HPos.CENTER); 
+            center_gp1[i].add(source1[i], 4, 1);
+            
+            destination1[i] = new Label(dest);
+            destination1[i].setStyle("-fx-font-size: 20px;"); 
+            GridPane.setHalignment(destination1[i], HPos.CENTER); 
+            center_gp1[i].add(destination1[i], 7, 1);
+            
+            //total time required
+            tot_time_req1[i] = new Label("2h 15m");
+            tot_time_req1[i].setStyle("-fx-font-size: 30px;-fx-font-weight: bold;");
+            GridPane.setHalignment(tot_time_req1[i], HPos.CENTER); 
+            center_gp1[i].add(tot_time_req1[i], 1, 5, 3, 1);
+            
+            //fare
+            fare1[i] = new Label("Rs. 4061");
+            fare1[i].setStyle("-fx-font-size: 30px;-fx-font-weight: bold;-fx-text-fill:red"); 
+            GridPane.setHalignment(fare1[i], HPos.CENTER); 
+            center_gp1[i].add(fare1[i], 6, 5, 2, 1);
+            
+            book_btn1[i] =new Button("Book");
+            //book_btn1[i].setStyle("-fx-font-size: 15px; -fx-background-color: #cccccc"); 
+            center_gp1[i].add(book_btn1[i], 8, 7, 2, 1);
+            
+            sp1[i] = new StackPane();
+            sp1[i].getChildren().addAll(r1[i], center_gp1[i]); 
+            v1.getChildren().add(sp1[i]); 
+            
+            /*if(trip=="Round Trip")
+            {
+                //setting primary properties required for gridPane
+            center_gp2[i] = new GridPane();
+            center_gp2[i].setAlignment(Pos.CENTER); 
+            
+            //background of gridPane
+            r2[i] = new Rectangle();
+            r2[i].setWidth(530); 
+            r2[i].setHeight(230);
+            r2[i].setStyle("-fx-fill:white;-fx-effect: innershadow( three-pass-box , rgba(0,0,0,0.7) , 6, 0.0 , 0 , 2 ); ");
+            
+            
+            //center_gp[i].setStyle("-fx-background-color: white");
+            //setting gaps between rows and columns of grid 
+            center_gp2[i].setHgap(10);
+            center_gp2[i].setVgap(10);
+            
+            //padding of gridpane in scene
+            center_gp2[i].setPadding(new Insets(5,5,5,5));
+        
+            //center_gp[i].setGridLinesVisible(true);
+            
+            //end of setting
+            
+            //contents inside gridPane
+            
+            logo2[i] = new Image(Window.class.getResourceAsStream("air-logo.png"));
+            logo_img_view2[i] = new ImageView(logo2[i]);
+        
+           
+            //setting size of image logo
+            logo_img_view2[i].setFitHeight(65);
+            logo_img_view2[i].setFitWidth(65);
+            GridPane.setHalignment(logo_img_view2[i], HPos.CENTER);
+            GridPane.setValignment(logo_img_view2[i], VPos.CENTER);
+            //adding logo image 
+            center_gp2[i].add(logo_img_view2[i],0,0);
+            
+            //Adding flight name 
+            flight_name2[i] = new Label("AeroSwing");
+            flight_name2[i].setStyle("-fx-font-size: 15px;");
+            GridPane.setHalignment(flight_name2[i], HPos.LEFT); 
+            GridPane.setValignment(flight_name2[i], VPos.CENTER);
+            center_gp2[i].add(flight_name2[i], 1, 0);
+            
+            time_dest2[i] = new Label("19:25");
+            time_dest2[i].setStyle("-fx-font-size: 30px;-fx-font-weight: bold;");
+            GridPane.setHalignment(time_dest2[i], HPos.CENTER); 
+            center_gp2[i].add(time_dest2[i], 4, 0);
+            
+            time_src2[i] = new Label("17:10");
+            time_src2[i].setStyle("-fx-font-size: 30px;-fx-font-weight: bold;");
+            GridPane.setHalignment(time_src2[i], HPos.CENTER); 
+            center_gp2[i].add(time_src2[i], 7, 0);
+            
+            destination2[i] = new Label(dest);
+            destination2[i].setStyle("-fx-font-size: 20px;"); 
+            GridPane.setHalignment(destination2[i], HPos.CENTER); 
+            center_gp2[i].add(destination2[i], 4, 1);
+            
+            source2[i] = new Label(src);
+            source2[i].setStyle("-fx-font-size: 20px;"); 
+            GridPane.setHalignment(source2[i], HPos.CENTER); 
+            center_gp2[i].add(source2[i], 7, 1);
+            
+            //total time required
+            tot_time_req2[i] = new Label("2h 15m");
+            tot_time_req2[i].setStyle("-fx-font-size: 30px;-fx-font-weight: bold;");
+            GridPane.setHalignment(tot_time_req2[i], HPos.CENTER); 
+            center_gp2[i].add(tot_time_req2[i], 1, 5, 3, 1);
+            
+            //fare
+            fare2[i] = new Label("Rs. 4061");
+            fare2[i].setStyle("-fx-font-size: 30px;-fx-font-weight: bold;-fx-text-fill:red"); 
+            GridPane.setHalignment(fare2[i], HPos.CENTER); 
+            center_gp2[i].add(fare2[i], 6, 5, 2, 1);
+            
+            book_btn2[i] =new Button("Book");
+            //book_btn2[i].setStyle("-fx-font-size: 15px; -fx-background-color: #cccccc"); 
+            center_gp2[i].add(book_btn2[i], 8, 7, 2, 1);
+            
+            sp2[i] = new StackPane();
+            sp2[i].getChildren().addAll(r2[i], center_gp2[i]); 
+            v2.getChildren().add(sp2[i]);
+            
+            }*/
+            
+        }
+        }catch(Exception ex) {
+            System.out.println(ex);
+        }
+        
+        v1.setStyle("-fx-background-color: #cccccc"); 
+        v2.setStyle("-fx-background-color: #cccccc"); 
+        
+        ScrollPane rootPane1 = new ScrollPane();
+        rootPane1.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        rootPane1.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS); 
+        rootPane1.setFitToHeight(true);
+        rootPane1.setFitToWidth(true); 
+        //rootPane1.setVmax(2);
+        //rootPane1.setHmax(2);
+        //rootPane1.setVvalue(20);
+        rootPane1.setContent(v1);  
+        
+        ScrollPane rootPane2 = new ScrollPane();
+        rootPane2.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        rootPane2.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS); 
+        rootPane2.setFitToHeight(true);
+        rootPane2.setFitToWidth(true); 
+        //rootPane2.setVmax(2);
+        //rootPane2.setHmax(2);
+        //rootPane2.setVvalue(20);
+        rootPane2.setContent(v2);
+        
+        hb.getChildren().addAll(rootPane1, rootPane2);
+        hb.setAlignment(Pos.CENTER); 
+        hb.setStyle("-fx-background-color: #999999");
+        
+        bp.setCenter(hb);
+        
+        //End of center of Bottom Pane
+        
+        //**Bottom of Border Pane**//
+        HBox hbox = new HBox();
+        
+        Button back_btn = new Button("Back");
+        back_btn.setPrefSize(300, 40);
+        back_btn.setId("btn"); 
+        
+        Button next_btn = new Button("Next");
+        next_btn.setPrefSize(300, 40); 
+        next_btn.setId("btn");
+        
+        hbox.getChildren().addAll(back_btn, next_btn);
+        hbox.setSpacing(100); 
+        hbox.setPadding(new Insets(20, 20, 20, 20)); 
+        hbox.setAlignment(Pos.CENTER); 
+        hbox.setStyle("-fx-background-color: #999999;");
+        bp.setBottom(hbox); 
+        
+        //End of Bottom of Border Pane
         
         fSearchScene = new Scene(bp, 1000, 600);
+        fSearchScene.getStylesheets().add(Flights.class.getResource("Flights.css").toExternalForm());
     }
 }
